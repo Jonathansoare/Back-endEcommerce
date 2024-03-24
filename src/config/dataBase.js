663ac9c4a182aec0eb84a682c0b6813b50bbcdaa
Jonathan Soares <98005459+Jonathansoare@users.noreply.github.com>
2024-03-23 22:42:50 -0300

    db

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,10 +1,19 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-
 const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
   host: process.env.DBHOST,
   dialect: 'mysql',
 });
+
+// Testar a conexão
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão bem-sucedida com o banco de dados.');
+  } catch (error) {
+    console.error('Erro ao conectar-se ao banco de dados:', error);
+  }
+})();
 
 module.exports = sequelize;
