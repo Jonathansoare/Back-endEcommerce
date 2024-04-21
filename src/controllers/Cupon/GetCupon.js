42cd5c4d05c6135d466669d01c5e4b6e530e93f9
Jonathan Soares <98005459+Jonathansoare@users.noreply.github.com>
2024-04-21 19:22:00 -0300

    UPDATE

--- src/controllers/Cupon/GetCupon.js
+++ src/controllers/Cupon/GetCupon.js
@@ -1,17 +1,29 @@
 const Cupons = require('../../models/Cupon');
 
-async function getCupon(req,res){
+async function getCupon(req, res) {
     try {
-        const Cupom = await Cupons.findAll({
-            attributes:['id','NameCupon','Status','ValorDesconto','DataValidade']
-        })
+        const id = req.params.id || null; // Verifica se há um parâmetro ID na solicitação
 
-        return res.status(200).json({
-            Cupom
-        })
+        let whereCondition = {}; // Condição de busca inicialmente vazia
+
+        if (id) {
+            whereCondition.id = id; // Se houver um ID, a busca será filtrada por esse ID
+        }
+
+        const cupons = await Cupons.findAll({
+            where: whereCondition, // Aplica a condição de busca, se houver
+            attributes: ['id', 'NameCupon', 'Status', 'ValorDesconto', 'DataValidade']
+        });
+
+        if (cupons.length === 0) {
+            return res.status(204).json({ message: 'Nenhum cupom encontrado' });
+        }
+
+        return res.status(200).json({ cupons });
     } catch (error) {
-        return res.status(500).json({ message: 'Ocorreu um erro ao buscar cupoms.' });
+        console.error(error);
+        return res.status(500).json({ message: 'Ocorreu um erro ao buscar cupons.' });
     }
 }
 
-module.exports = {getCupon}
+module.exports = { getCupon };
