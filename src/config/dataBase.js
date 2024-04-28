bbb25cf98174385d36f4b9a2a85c8d868b445327
Jonathan Soares <98005459+Jonathansoare@users.noreply.github.com>
2024-04-27 23:36:59 -0300

    CONFIG DB

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DBNAME || 'techshop', 
   process.env.DBUSER || 'root', 
   process.env.DBPASSWORD || '90351222', {
-  host: process.env.DBHOST || 'localhost',
+  host: process.env.DBHOST || '127.0.0.1',
   port:process.env.DBPORT || '3306',
   dialect: 'mysql',
 });
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Conexão bem-sucedida com o banco de dados.');
+    console.log(`Conexão bem-sucedida com o banco de dados.  rondando na porta: ${host}`);
   } catch (error) {
     console.log(`dados: DBNAME:${process.env.DBNAME}, DBUSER:${process.env.DBUSER}, DBPASSWORD:${process.env.DBPASSWORD},DBHOST:${process.env.DBHOST}, DBPORT:${process.env.DBPORT}`)
     console.error('Erro ao conectar-se ao banco de dados:', error);
