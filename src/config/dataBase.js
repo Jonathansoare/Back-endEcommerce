d793ba34160f1785ee5c8620e619266ffd0aa48a
Jonathan Soares <98005459+Jonathansoare@users.noreply.github.com>
2024-04-27 23:27:31 -0300

    CONFIG

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,9 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
-  host: process.env.DBHOST,
-  port:process.env.DBPORT,
+const sequelize = new Sequelize(
+  process.env.DBNAME || 'techshop', 
+  process.env.DBUSER || 'root', 
+  process.env.DBPASSWORD || '90351222', {
+  host: process.env.DBHOST || 'localhost',
+  port:process.env.DBPORT || '3306',
   dialect: 'mysql',
 });
 
